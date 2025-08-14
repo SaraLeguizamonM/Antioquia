2e70aa489f57d636e530bae57dd198837f563fce
SARA NICOLLE LEGUIZAMON MORENO <sara.leguizamon@inemjose.edu.co>
2025-08-14 04:28:58 +0000

    ahora pasame un codigo para el cmd donde me crees un archivo out (tood l

---
+++ src/next.config.ts
@@ -0,0 +1,55 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.colombia.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn0.recetasgratis.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.wikia.nocookie.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.chilango.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'losmitosyleyendas.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.antioquiacritica.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.weather-forecast.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
